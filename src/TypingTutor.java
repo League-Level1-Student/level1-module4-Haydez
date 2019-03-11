7a898bb65d0401ca78f1506413af643f5df18747
league <league@wts5.attlocal.net>
2019-03-10 17:25:21 -0700

    Typingtutor not finish

--- src/TypingTutor.java
+++ src/TypingTutor.java
@@ -1,12 +1,20 @@
+import java.util.Random;
+
 import javax.swing.*;
 
 public class TypingTutor {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
 	
 	public void makeGUI() {
 		frame.setVisible(true);
 		frame.add(panel);
 		frame.setTitle("Type Or Die");
+		frame.pack();
+		
+		label.setFont(label.getFont().deriveFont(28.0f));
+		label.setHorizontalAlignment(JLabel.CENTER);
 	}
+	
 }
