3ee9d12e21d9872b05e46f74a0f1fba8f29a5af3
alvincvms <48074117+alvincvms@users.noreply.github.com>
2019-04-21 17:25:35 -0700

    calculatorpakl hFFgukjhvf szhgdvzku˙≈©kuyhzgdchgp;awuiogdhlkjcgzlliursgijakjlcbvali

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,9 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-public class Calculator {
+public class Calculator implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton add = new JButton();
@@ -14,10 +17,32 @@ public class Calculator {
 		frame.setVisible(true);
 		frame.setSize(500, 500);
 		frame.add(panel);
-		frame.add(x);
-		frame.add(y);
-		
-		
+		panel.add(x);
+		x.setText("PEEPEE hard");
+		panel.add(y);
+		y.setText("peepee soft");
+		panel.add(add);
+		panel.add(subtract);
+		panel.add(divide);
+		panel.add(multiply);
+		add.addActionListener(this);
+		frame.pack();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource().equals(add)) {
+			
+		}
+		if(e.getSource().equals(subtract)) {
+			
+		}
+		if(e.getSource().equals(multiply)) {
+			
+		}
+		if(e.getSource().equals(divide)) {
+			
+		}
 	}
 	
 }
