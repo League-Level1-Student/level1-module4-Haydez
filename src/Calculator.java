407032b01ea6b0e577d61dcb82573b5636da7d28
league <league@wts5.attlocal.net>
2019-04-28 17:25:05 -0700

    finish calculator and finish twitter kahsdv,msz fv xfb

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,36 +13,55 @@ public class Calculator implements ActionListener {
 	JButton calculate = new JButton();
 	JTextField x = new JTextField();
 	JTextField y = new JTextField();
+	JLabel answer = new JLabel();
 	public void makeGUI() {
 		frame.setVisible(true);
 		frame.setSize(500, 500);
 		frame.add(panel);
 		panel.add(x);
-		x.setText("PEEPEE hard");
+		x.setText("     ");
 		panel.add(y);
-		y.setText("peepee soft");
+		y.setText("     ");
 		panel.add(add);
 		panel.add(subtract);
 		panel.add(divide);
 		panel.add(multiply);
+		panel.add(answer);
+		answer.setSize(10, 10);
+		add.setText("+");
+		subtract.setText("-");
+		multiply.setText("x");
+		divide.setText("÷"); 
 		add.addActionListener(this);
+		subtract.addActionListener(this);
+		multiply.addActionListener(this);
+		divide.addActionListener(this);
 		frame.pack();
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		int a = Integer.parseInt(x.getText().trim());
+		int b = Integer.parseInt(y.getText().trim());
+	
+		
 		if(e.getSource().equals(add)) {
-			
+			int sum = a + b;
+			answer.setText("" + sum);
 		}
 		if(e.getSource().equals(subtract)) {
-			
+			int sub = a - b;
+			answer.setText("" + sub);
 		}
 		if(e.getSource().equals(multiply)) {
-			
+			int mult =  a * b;
+			answer.setText("" + mult);
 		}
 		if(e.getSource().equals(divide)) {
-			
+			int div = a / b;
+			answer.setText("" + div);
 		}
+		frame.pack();
 	}
 	
 }
---
+++ src/twitterDriver.java
@@ -0,0 +1,7 @@
+
+public class twitterDriver {
+public static void main(String[] args) {
+	twitter GUI = new twitter();
+	GUI.makeGUI();
+}
+}
